1bdd96d2a425f32bb8ef191a21487809a440dc6d
cavenegas2 <cavenegas2@espe.edu.ec>
2021-03-09 11:08:52 -0500

    area del triangulo

--- src/Implementaciones.java
+++ src/Implementaciones.java
@@ -7,8 +7,23 @@ public class Implementaciones {
 		double aux = 0;
 		
 		aux = lado*lado;
+		System.out.println(lado);
+		System.out.println("El area del cuadrado es: ");
 		
 		return aux;
+		
+	}
+	
+	
+	
+	
+	public double calculartriangulo(double base , double altura) {
+		double aux2 = 0;
+		
+		aux2=((base*altura)/2);
+		System.out.println("La base es igual a: "+base +"y la altura es: " +altura);
+		System.out.println("El area triangulo es: ");
+		return aux2;
 		
 	}
 
